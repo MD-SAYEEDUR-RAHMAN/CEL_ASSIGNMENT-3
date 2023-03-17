615b682691c449ab9bc5686dd733b1ea5a04a317
MD-SAYEEDUR-RAHMAN <saidurnohan@gmail.com>
2023-03-18 03:58:28 +0600

    Third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'student-project';
+  firstName: string = '';
+  lastName: string = '';
+  progRam: string = '';
+  idNo: string = '';
+  cgPA: string = '';
+  students: Student[] = [];
+
+  submit() {
+    const student = { firstName: this.firstName, lastName: this.lastName, progRam: this.progRam , idNo: this.idNo,cgPA: this.cgPA};
+    this.students.push(student);
+    this.firstName = '';
+    this.lastName = '';
+    this.progRam = '';
+    this.idNo = '';
+    this.cgPA = '';
+  }
+}
+
+class Student {
+  firstName: string = '';
+  lastName: string = '';
+  progRam: string = '';
+  idNo: string = '';
+  cgPA: string = '';
+
 }
